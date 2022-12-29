4695613783a8478612b50e5fe945e88e480e00cd
gosadchuk <gosadchuk@ozon.ru>
2022-12-29 23:42:03 +0300

    [soa] feat: service

---
+++ service/api/api.go
@@ -0,0 +1,86 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type (
+	APICfg struct {
+		Address string
+		Handles []handle
+	}
+
+	api struct {
+		handles HandleMap
+	}
+
+	API interface {
+		DoRequest(ctx context.Context, handle string, body []byte) (*http.Response, error)
+	}
+
+	handle interface {
+		GetName() string
+		GetMethod() string
+		GetURL() string
+	}
+
+	HandleMap map[string]handleFunc
+
+	handleFunc func(context.Context, []byte) (*http.Response, error)
+)
+
+var NotFoundHandle = errors.New("handle not found")
+
+func NewAPI(cfg APICfg) API {
+	handlesMap := make(map[string]handleFunc, len(cfg.Handles))
+	for _, handle := range cfg.Handles {
+		method := handle.GetMethod()
+		url := cfg.Address + handle.GetURL()
+		handlesMap[handle.GetName()] = func(ctx context.Context, body []byte) (*http.Response, error) {
+			cl := &http.Client{}
+			req, err := http.NewRequestWithContext(ctx, method, url, bytes.NewReader(body))
+			if err != nil {
+				return nil, err
+			}
+			resp, err := cl.Do(req)
+			if err != nil {
+				return nil, err
+			}
+
+			return resp, nil
+		}
+	}
+
+	return &api{handles: handlesMap}
+}
+
+func (a api) DoRequest(ctx context.Context, handle string, body []byte) (*http.Response, error) {
+	if f, ok := a.handles[handle]; ok {
+		return f(ctx, body)
+	}
+	return nil, NotFoundHandle
+}
+
+func UnmarshalBody[StructType any](resp *http.Response) (StructType, error) {
+	defer resp.Body.Close()
+
+	var out StructType
+	body, err := io.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &out)
+
+	return out, err
+}
+
+func GetHandels[T handle](handles []T) []handle {
+	res := make([]handle, len(handles))
+	for i, val := range handles {
+		res[i] = handle(val)
+	}
+	return res
+}
---
+++ service/server/service.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	// _ "github.com/soa/catalog-api/docs"
+
+	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+type server struct {
+	gin         *gin.Engine
+	address     string
+	controllers []controller
+}
+
+func NewServer(
+	address string,
+	controllers ...controller,
+) Server {
+	srv := &server{
+		gin:         gin.Default(),
+		address:     address,
+		controllers: controllers,
+	}
+
+	srv.setHandlers()
+
+	return srv
+}
+
+// @title   Catalog API
+// @version 1.0
+
+// @host     localhost:7003
+// @BasePath /api/v1
+func (s *server) setHandlers() {
+	v1 := s.gin.Group("/api/v1")
+	{
+		for _, c := range s.controllers {
+			c.SetV1Handlers(v1)
+		}
+	}
+
+	s.gin.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+}
+
+func (s *server) Run() error {
+	return s.gin.Run(s.address)
+}
